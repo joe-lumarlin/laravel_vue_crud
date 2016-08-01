2a3978ac17887f58752a46e1c8e328fd37a41bcd
Joe Lumarlin <jlumarlin@gmail.com>
2016-08-01 12:50:00 +0300

    Articles timestamp getters

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon;
 class Article extends Model
 {
     protected $fillable = ['title', 'content'];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('d.m.Y.');
+    }
+
+    public function getUpdatedAtAttribute($date)
+    {
+        return Carbon::parse($date)->format('d.m.Y');
+    }
 }
